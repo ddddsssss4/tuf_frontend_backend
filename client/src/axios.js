a4135ec10f05b9ce4ce18962ff65443e4e06beef
ddddsssss4 <saraswatdevesh98@gmail.com>
2024-08-13 21:35:02 +0530

    Some issue resolved with backend

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Adjust to your backend URL
+  baseURL: 'https://takeuforward-assignment-8kd8.onrender.com/api/v1', // Adjust to your backend URL
 });
 
 export default instance;
